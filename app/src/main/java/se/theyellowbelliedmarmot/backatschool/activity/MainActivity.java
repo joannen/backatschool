dd0d631f9038f1b631ef844cde8aec032e894d6b
Anna Andersson <anna@snigelfart.se>
2016-06-02 20:31:19 +0200

    created save and read user. Saving user in shared pref.

--- app/src/main/java/se/theyellowbelliedmarmot/backatschool/activity/MainActivity.java
+++ app/src/main/java/se/theyellowbelliedmarmot/backatschool/activity/MainActivity.java
@@ -1,6 +1,8 @@
 package se.theyellowbelliedmarmot.backatschool.activity;
 
+import android.content.Context;
 import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
@@ -32,36 +34,56 @@ public class MainActivity extends AppCompatActivity {
 //            finish();
 //        }
 
-        final EditText authFirstName = new EditText(this);
-        final EditText authLastName = new EditText(this);
-        final AlertDialog.Builder authInputBox = new AlertDialog.Builder(MainActivity.this)
-                .setTitle("Enter your'e name: ")
-                .setView(authFirstName)
-                .setView(authLastName)
-                .setCancelable(false)
-                .setPositiveButton("Send", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        firstName = authFirstName.getText().toString();
-                        lastName = authLastName.getText().toString();
-                        user = new User(firstName, lastName);
-                        try {
-                            userService.registerUser(user, getApplicationContext());
-                        } catch (Exception e){
-                            e.printStackTrace();
+        if(readUser().equals(""))
+            {final EditText authFirstName = new EditText(this);
+            final EditText authLastName = new EditText(this);
+            final AlertDialog.Builder authInputBox = new AlertDialog.Builder(MainActivity.this)
+                    .setTitle("Enter your'e name: ")
+                    .setView(authFirstName)
+                    .setView(authLastName)
+                    .setCancelable(false)
+                    .setPositiveButton("Check In", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            firstName = authFirstName.getText().toString();
+                            lastName = authLastName.getText().toString();
+                            user = new User(firstName, lastName);
+                            try {
+                                saveUser(firstName, lastName);
+                                userService.registerUser(user, getApplicationContext());
+                            } catch (Exception e){
+                                e.printStackTrace();
+                            }
                         }
-                    }
-                })
-                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        dialogInterface.cancel();
-                        dialogInterface.dismiss();
-                        finish();
-                    }
-                });
-        authInputBox.create();
-        authInputBox.show();
+                    })
+                    .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            dialogInterface.cancel();
+                            dialogInterface.dismiss();
+                            finish();
+                        }
+                    });
+            authInputBox.create();
+            authInputBox.show();
+        }
+//        else {
+            // Create new Intent and start Scan Activity
+//        }
+    }
 
+    private void saveUser(String firstName, String lastName){
+        SharedPreferences sharedPreferences = this.getPreferences(Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("first_name", firstName);
+        editor.putString("last_name", lastName);
+        editor.commit();
+    }
+
+    private String readUser(){
+        SharedPreferences sharedPreferences = this.getPreferences(Context.MODE_PRIVATE);
+        String firstName = sharedPreferences.getString("first_name","");
+        String lastName = sharedPreferences.getString("last_name", "");
+        return  firstName + lastName;
     }
 }
